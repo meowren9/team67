61fe713bd59c796223aeadbbd7f0bf3a3144d8c6
meow.ren9 <meow.ren9@gmail.com>
2017-11-12 21:31:26 -0500

    neck rotation

--- Assets/Scripts/Battle/Nian/HeadLookAt.cs
+++ Assets/Scripts/Battle/Nian/HeadLookAt.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class HeadLookAt : MonoBehaviour {
 
     public Transform target;
+    public Transform head;
     public float speed;
-	void Update () {
+
+    private void Start()
+    {
+        //head.eulerAngles += new Vector3(-9.211f, 90, 0);
+        transform.forward = Vector3.left;
+        //transform.eulerAngles += new Vector3(0, 90, 0);
+    }
+
+    void Update () {
         var targetRotation = Quaternion.LookRotation(target.position - transform.position);
         //TODO: need constraint
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
-	}
+        //transform.rotation += Quaternion()
+        //head.eulerAngles += new Vector3(0, 90, 0);
+
+    }
 }
