8a0ab25d3bbd3e799e55efac08d8989b80a22c87
SeeGeer <rensjzju@hotmail.com>
2017-11-18 01:02:24 -0500

    Lantern release

---
+++ Assets/Scripts/Lantern/VillagerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillagerController : MonoBehaviour {
+
+    public Transform inside;
+    public Transform outside;
+    public Transform inTheMiddle;
+    public Transform lanternPosition;
+    public Transform peoplePosition;
+
+    public float speedLantern = 0.2f;
+    public float speedVillager = 0.5f;
+    public float speedLanternUp = 0.5f;
+
+    public GameObject lantern;
+    public GameObject people;
+
+    float t = 0.0f;
+    public float minimum = -1.0f;
+    public float maximum = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+        if(t < 1.0f)
+        {
+            this.transform.position = Vector3.Lerp(inside.position, outside.position, t);
+            t += speedVillager * Time.deltaTime * Random.Range(0.2f, 3.0f);
+        }
+
+
+        else if(t > 1.0f && t < 2.0f)
+        {
+            lantern.transform.position = Vector3.Lerp(lanternPosition.position, inTheMiddle.position, t - 1);
+            people.transform.position = Vector3.Lerp(peoplePosition.position, inside.position, t - 1);
+            
+            t += speedVillager * Time.deltaTime * Random.Range(0.2f, 3.0f);
+
+        }
+        
+
+        else if(t > 2.0f && t < 3.0f)
+        {
+            Destroy(people, 0.1f);
+            lantern.transform.position += new Vector3(0.0f, 0.4f * speedLanternUp * Random.Range(0.2f, 3.0f), 0.0f);
+        }
+
+
+
+
+    }
+}
---
+++ Assets/Scripts/Lantern/VillagerCreater.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillagerCreater : MonoBehaviour {
+
+
+    public GameObject[] VillagerWithLantern;
+
+    public Transform[] presetPositon;
+    public int VillagersPerUnitTime = 10;
+    public int lanternTypes;
+    public int villagerTypes;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(CreateVillager());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    IEnumerator CreateVillager()
+    {
+        while (true)
+        {
+            
+            for (int i = 0; i < VillagersPerUnitTime; i++)
+            {
+                int PositionNumber = Random.Range(0, 21);
+                int VillagerNumber = Random.Range(0, 2);
+                GameObject.Instantiate(VillagerWithLantern[VillagerNumber], presetPositon[PositionNumber]);
+            }
+                
+
+
+            yield return new WaitForSeconds(1.0f);
+        }
+        
+    }
+}
