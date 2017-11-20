05d1caffd185e6e34362882cda47551cad860761
Yidi <zhuyidiwow@gmail.com>
2017-11-20 13:35:31 -0500

    change bgm manager

--- Assets/Scripts/BGMManager.cs
+++ Assets/Scripts/BGMManager.cs
@@ -2,38 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BGMManager : MonoBehaviour {
-
+public class BGMManager : MonoBehaviour, IPunObservable
+{
+    public int current_bgm_index = 0;
     public AudioClip[] bgms;
     AudioSource my_audio;
-    public GameManager gm;
-
-    int prev_gm_status;
-
-    private void Start()
-    {
-        prev_gm_status = gm.status;
-        my_audio = GetComponent<AudioSource>();
-        my_audio.clip = bgms[0];
-        my_audio.Play();
-        prev_gm_status = gm.status;
-    }
-
 
     private void Update()
     {
-        int cur = gm.status;
-        if(cur != prev_gm_status)
-        {
-            PlayMusic(cur);
-            prev_gm_status = cur;
-        }
+        PlayMusic();
     }
 
-    void PlayMusic(int index)
+    void PlayMusic()
     {
+        if (my_audio.clip == bgms[current_bgm_index])
+            return;
+
         my_audio.Stop();
-        my_audio.clip = bgms[index];
+        my_audio.clip = bgms[current_bgm_index];
         my_audio.Play();
+    }
+
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(current_bgm_index);
+        }
+        else
+        {
+            this.current_bgm_index = (int)stream.ReceiveNext();
+        }
     }
 }
